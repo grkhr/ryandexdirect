3db6e0950656ca9a7e84d55875e883646b03664b
grkhr <eduardgorkh@gmail.com>
2019-12-01 18:17:10 +0300

    Update yadirGetClientParam.R

--- R/yadirGetClientParam.R
+++ R/yadirGetClientParam.R
@@ -1,22 +1,22 @@
-yadirGetClientParam <- function(Language = "ru", 
-                                Logins = NULL, 
+yadirGetClientParam <- function(Language = "ru",
+                                Logins = NULL,
                                 Token = NULL,
                                 AgencyAccount = NULL,
                                 TokenPath     = getwd()){
-  
-  
-  #Авторизация
+
+
+  #???????????
   Token <- tech_auth(login = Logins, token = Token, AgencyAccount = AgencyAccount, TokenPath = TokenPath)
-  
-  #Загрузка списка клиентов если небыло задани их список
+
+  #???????? ?????? ???????? ???? ?????? ?????? ?? ??????
   if ( ! is.null(AgencyAccount) && is.null(Logins)) {
     Logins <- yadirGetClientList(AgencyAccount = AgencyAccount, TokenPath = TokenPath, Token = Token)$Login
   }
-  
-  #Формируем запрос
+
+  #????????? ??????
     queryBody <- paste0("{
   \"method\": \"get\",
-                      \"params\": { 
+                      \"params\": {
                       \"FieldNames\": [
                       \"AccountQuality\",
                       \"Archived\",
@@ -36,28 +36,28 @@ yadirGetClientParam <- function(Language = "ru",
                       \"VatRate\"]
       }
     }")
-  
+
   result <- data.frame()
-  
+
   for (login in Logins) {
-    
+
     #Ioi?aaea cai?ina ia na?aa?
     answer <- POST("https://api.direct.yandex.com/json/v5/clients", body = queryBody, add_headers(Authorization = paste0("Bearer ",Token), 'Accept-Language' = Language, "Client-Login" = login))
     #I?iaa?ea ?acoeuoaoa ia ioeaee
     stop_for_status(answer)
-    
+
     dataRaw <- content(answer, "parsed", "application/json")
-    
+
     if(length(dataRaw$error) > 0){
       warning(login,paste0(": ",dataRaw$error$error_string, " - ", dataRaw$error$error_detail))
 	  next
     }
-    
+
     #I?aia?acoai ioaao a data frame
-    
+
     #Ia?neia ni?aai?ieea ?aaeiiia
       dictionary_df <- data.frame()
-      
+
       for(dr in 1:length(dataRaw$result[[1]])){
         dictionary_df_temp <- data.frame(Login = dataRaw$result[[1]][[dr]]$Login,
                                          ClientId = dataRaw$result[[1]][[dr]]$ClientId,
@@ -69,22 +69,22 @@ yadirGetClientParam <- function(Language = "ru",
                                          CampaignsTotalPerClient = dataRaw$result[[1]][[dr]]$Restrictions[[1]]$Value,
                                          CampaignsUnarchivePerClient = dataRaw$result[[1]][[dr]]$Restrictions[[2]]$Value,
                                          APIPoints = dataRaw$result[[1]][[dr]]$Restrictions[[10]]$Value)
-        
+
         dictionary_df <- rbind(dictionary_df, dictionary_df_temp)
-        
+
       }
-      
+
 
 
     #Auaiaei eioi?iaoe? i ?aaioa cai?ina e i eiee?anoaa aaeeia
      #packageStartupMessage("Ni?aai?iee oniaoii caa?o?ai!", appendLF = T)
-     packageStartupMessage(paste0("Баллы списаны с : " ,answer$headers$`units-used-login`), appendLF = T)
-     packageStartupMessage(paste0("К-во баллов израсходованых при выполнении запроса: " ,strsplit(answer$headers$units, "/")[[1]][1]), appendLF = T)
-     packageStartupMessage(paste0("Доступный остаток суточно о лимита баллов: " ,strsplit(answer$headers$units, "/")[[1]][2]), appendLF = T)
-     packageStartupMessage(paste0("Суточный лимит баллов: " ,strsplit(answer$headers$units, "/")[[1]][3]), appendLF = T)
-     packageStartupMessage(paste0("Уникальный идентификатоо запроса который необходимо указывать при обращении в службу поддержки: ",answer$headers$requestid), appendLF = T)
+     packageStartupMessage(paste0("gseegseg: " ,answer$headers$`units-used-login`), appendLF = T)
+     packageStartupMessage(paste0("esgegs: " ,strsplit(answer$headers$units, "/")[[1]][1]), appendLF = T)
+     packageStartupMessage(paste0("sgeegegs: " ,strsplit(answer$headers$units, "/")[[1]][2]), appendLF = T)
+     packageStartupMessage(paste0("esgegs: " ,strsplit(answer$headers$units, "/")[[1]][3]), appendLF = T)
+     packageStartupMessage(paste0("esgesgesg: ",answer$headers$requestid), appendLF = T)
 
-     # прикрепляем к общему результату
+     # ??????????? ? ?????? ??????????
      result <- rbind(result, dictionary_df)
   }
   return(result)
